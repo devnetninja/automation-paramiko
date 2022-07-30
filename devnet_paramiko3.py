d41366ba9c68c1edb39d75040a30a272a949a550
devnetninja <devnetninja@gmail.com>
2022-07-30 11:03:14 -0700

    add devnet_paramiko3.py and routers2.py (for working at coffee shop)

---
+++ devnet_paramiko3.py
@@ -0,0 +1,14 @@
+from myparamiko import connect, get_shell, send_command, show, close
+from routers2 import r1, r2, r3
+
+client_connection = connect(**r1)
+shell = get_shell(client_connection)
+
+send_command(shell, 'enable')
+send_command(shell, 'cisco')
+send_command(shell, 'show version')
+
+output = show(shell)
+print(output)
+
+close(shell)
---
+++ routers2.py
@@ -0,0 +1,9 @@
+r1 = {"hostname":"192.168.110.151", "port":"22", "username":"cisco", "password":"cisco"}
+r2 = {"hostname":"192.168.110.152", "port":"22", "username":"cisco", "password":"cisco"}
+r3 = {"hostname":"192.168.110.153", "port":"22", "username":"cisco", "password":"cisco"}
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
